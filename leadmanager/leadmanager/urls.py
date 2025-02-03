3c588a47895ab6ca00d4478148281c5b078bea7a
jaini-ganesh <jaini.ganesh@gmail.com>
2025-02-02 20:20:02 -0500

    Updtated Component Commit

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -20,5 +20,6 @@ import leads
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('leads.urls'))
+    path('',include('frontend.urls')),
+    path('',include('leads.urls')),
 ]
